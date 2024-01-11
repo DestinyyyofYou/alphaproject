d02f2103e4d8a6e885557470b4a9d7989baeb7d5
DestinyyyofYou <lethiendinh123@gmail.com>
2024-01-12 03:56:47 +0700

    config

--- index.js
+++ index.js
@@ -3,15 +3,54 @@ const tankButton = document.querySelector(".center_tank");
 const lightButton = document.querySelector(".center_Lightweight");
 const acceptButton = document.querySelector(".footer_rectangleAccept");
 const declineButton = document.querySelector(".footer_rectangleDecline");
-
+var option;
 const runnerHandler = () => {
-    alert("Click runner");
+    $('.center_runner').hover(
+        function () {
+            $(this).addClass('hovered');
+        },
+        function () {
+            $(this).removeClass('hovered');
+        }
+    );
+    $('.center_runner').one('click',function () {
+        $(this).toggleClass('clicked');
+        console.log('a');
+    })
+    $('.center_runner').keyup(function (event) {
+        if (event.key === 'r') {
+            $(this).toggleClass('clicked');
+        }
+    });
+    // alert('Runner clicked');
 };
 const tankHandler = () => {
-    alert("Click tank");
+    $('.center_tank').hover(
+        function () {
+            $(this).addClass('hovered');
+        },
+        function () {
+            $(this).removeClass('hovered');
+        }
+    );
+    $('.center_tank').click(function () {
+        $(this).toggleClass('clicked');
+        alert('Tank clicked');
+    })
 };
 const lightHandler = () => {
-    alert("Click light");
+    $('.center_Lightweight').hover(
+        function () {
+            $(this).addClass('hovered');
+        },
+        function () {
+            $(this).removeClass('hovered');
+        }
+    );
+    $('.center_Lightweight').click(function () {
+        $(this).toggleClass('clicked');
+        alert('Lightweight clicked');
+    })
 };
 runnerButton.addEventListener("click", () => {
     runnerHandler();
@@ -49,39 +88,9 @@ window.addEventListener("keyup", (event) => {
     }
 });
 
-$('.center_runner').hover(
-    function () {
-        $(this).addClass('hovered');
-    },
-    function () {
-        $(this).removeClass('hovered');
-    }
-);
-$('.center_runner').click(function () {
-    $(this).toggleClass('clicked');
-    alert('Runner clicked');
-})
-$('.center_tank').hover(
-    function () {
-        $(this).addClass('hovered');
-    },
-    function () {
-        $(this).removeClass('hovered');
-    }
-);
-$('.center_tank').click(function () {
-    $(this).toggleClass('clicked');
-    alert('Tank clicked');
-})
-$('.center_Lightweight').hover(
-    function () {
-        $(this).addClass('hovered');
-    },
-    function () {
-        $(this).removeClass('hovered');
-    }
-);
-$('.center_Lightweight').click(function () {
-    $(this).toggleClass('clicked');
-    alert('Lightweight clicked');
-})
+
+
+
+
+
+
